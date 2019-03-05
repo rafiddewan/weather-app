b3d02ab8ec929cbb54c16a7e70535ac04bba59f0
rafiddewan <rafid.dewan145@gmail.com>
2019-03-05 00:37:35 -0500

    Finished app.post, the application is fully functional with the UI

--- server.js
+++ server.js
@@ -1,18 +1,37 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const request = require('request');
 const app = express()
+
+const apiKey = '2c48c76726a4c5f039ad2d122c0acaa8';
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs')
 
 app.get('/', function (req, res) {
-  res.render('index');
+  res.render('index', {weather: null, error: null});
 })
+
 app.post('/', function (req, res) {
-  console.log(req.body.city);
-  res.render('index');  
+  let city = req.body.city;
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+
+  request(url, function (err, response, body) {
+    if(err){
+      res.render('index', {weather: null, error: 'Error, please try again'});
+    } else {
+      let weather = JSON.parse(body)
+      if(weather.main == undefined){
+        res.render('index', {weather: null, error: 'Error, please try again'});
+      } else {
+        let weatherText = `It's ${weather.main.temp} degrees Celsius in ${weather.name}!`;
+        res.render('index', {weather: weatherText, error: null});
+      }
+    }
+  });
 })
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
